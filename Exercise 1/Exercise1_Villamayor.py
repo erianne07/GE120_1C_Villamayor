809683e190e4dbaeeef653434f266c8ee8a8d175
Erianne Deniell H. Villamayor <ehvillamayor1@up.edu.ph>
2024-02-27 17:19:54 +0800

    Exercise 1

--- Exercise 1/Exercise1_Villamayor.py
+++ Exercise 1/Exercise1_Villamayor.py
@@ -4,23 +4,23 @@ Erianne Villamayor
 Machine Exercise 1
 """
 
-# dms = 118.42069
+dms = 118.42069
 
-# #Kunin si degree part
-# degree = int(dms)
-# print("degree", 118)
+#Kunin si degree part
+degree = int(dms)
+print("degree", 118)
 
-# #Kunin si minutes
-# minutes = (dms - degree) * 60
+#Kunin si minutes
+minutes = (dms - degree) * 60
 
-# minutes_fractional = int(minutes)
+minutes_fractional = int(minutes)
 
-# seconds = (minutes - minutes_fractional) * 60
+seconds = (minutes - minutes_fractional) * 60
 
-# print("minutes pero naka degree pa", minutes_fractional)
-# print("seconds pero naka degree pa", seconds)
+print("minutes pero naka degree pa", minutes_fractional)
+print("seconds pero naka degree pa", seconds)
 
-# print("DMS:" + str(degree) + "-" + str(minutes_fractional) + "-" + str(round(seconds, 2)))
+print("DMS:" + str(degree) + "-" + str(minutes_fractional) + "-" + str(round(seconds, 2)))
 
 # Must be a string input
 
@@ -32,6 +32,7 @@ degrees = int(values[0])
 minutes = int(values[1])
 seconds = float(values[2])
 
+
 dd = degrees + (minutes/60) + (seconds/3600)
 
-print("ETO YUNG VALUE:", round(dd,6))
+print("ETO YUNG VALUE NG DD:", round(dd,6))
