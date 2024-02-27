314838c9620b6694ebc68381b9f075367a8f6de2
Erianne Deniell H. Villamayor <ehvillamayor1@up.edu.ph>
2024-02-27 17:04:02 +0800

    Submit Exercise 1

--- Exercise 1/Exercise1_Villamayor.py
+++ Exercise 1/Exercise1_Villamayor.py
@@ -3,3 +3,35 @@ GE 120: Intro
 Erianne Villamayor
 hi
 """
+
+# dms = 118.42069
+
+# #Kunin si degree part
+# degree = int(dms)
+# print("degree", 118)
+
+# #Kunin si minutes
+# minutes = (dms - degree) * 60
+
+# minutes_fractional = int(minutes)
+
+# seconds = (minutes - minutes_fractional) * 60
+
+# print("minutes pero naka degree pa", minutes_fractional)
+# print("seconds pero naka degree pa", seconds)
+
+# print("DMS:" + str(degree) + "-" + str(minutes_fractional) + "-" + str(round(seconds, 2)))
+
+#Must be a string input
+
+dms = "118-25-14.48"
+values= dms.split('-')
+
+
+degrees = int(values[0])
+minutes = int(values[1])
+seconds = float(values[2])
+
+dd = degrees + (minutes/60) + (seconds/3600)
+
+print("ETO YUNG VALUE:", round(dd,6))
