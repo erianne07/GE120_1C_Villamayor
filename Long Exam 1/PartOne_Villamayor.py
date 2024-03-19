3428c090a6edd16984c8d6099f01c18210551916
Erianne Deniell H. Villamayor <ehvillamayor1@up.edu.ph>
2024-03-19 15:49:21 +0800

    VILLAMAYOR_Exam starts here

---
+++ Long Exam 1/PartOne_Villamayor.py
@@ -0,0 +1,32 @@
+"""
+1. The difference between procedural programming and functional programming, 
+is that one primarily uses functions like float, string, type, and the likes in coding,
+while the other is mostly focused on procedures as the name suggests, examples of these are if while and elif. 
+It is important to know what you need to do/code in order to be able to use the right type of programming.
+
+2. Git is a helpful tool in programming because it enables users to have a collaborative working environment
+with the use of the internet. By creating repositories, you can upload you code to your github account, where others 
+can access this and apply changes or revisions without the need of having to download any extra files, this makes coding projects 
+infinitely faster. Just knowing the basics like committing, pushing, pulling, and the likes is all you are required to know in order to use
+github to its full extent. 
+
+3.  While loops can be used when describing something especially when using True or False variables. It is especially helpful in determining
+whether what the user inputted is the information being asked or required. If yes then the loop will move on, if not then the loop will ask the
+initial question once again. For "for loops" these loops are used to specify a value or a function to be used.
+
+4. The Divide and Conquer paradigm in programming, in simpler terms,
+is a way to solve complex problems by breaking it down into smaller subproblems so that it can be easier or manageable to create solutions for.
+Basically it provides steps on what to do first so that we won't be going in blindly into a problem. As a GE student, an example for that
+in the real world is when we do our traverse or leveling activities. We don't immediately jump to conclusions or report making.
+We first divide all the tasks that is needed to be accomplished. Before starting we have to borrow instruments, and then set up, record/measure the data, and so on
+in this way, if ever we make an error or mistake it is easier to pinpoint what part in the activity we have to go back to in order to adjust and solve the error that was created. 
+The same can be said in programming, we divide tasks and procedures so that we have a clearer picture on what to do to achieve our end goal. In this way it is less confusing
+and it would be easier to go back to what made the error in the first place, if we go through all the steps and divisions instead on if we go straight from
+problem to solution.
+
+5. A task that is related to geomatics that is normally done manually but can be optimized by programming is elevation activities. 
+Like measureing for the horizontal distances of an area, leveling uses traverse data wherein in instead of establishing horizontal points to get the area.
+We instead use elevation to measure how far the instrument is from one stadia rod to another. The approach that can be done for this solution is similar to
+how we code traverse givens. But instead of the azimuths, bearings, lat, and dep. We can input the upper, lower, and middle readings of the stadia rod to compute
+for distance.
+"""
